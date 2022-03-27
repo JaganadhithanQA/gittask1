f0879893df8ee47dc28dced074694133194bd78b
JaganadhithanQA <jagancahcet@gmail.com>
2022-03-26 19:33:03 -0700

    new task

---
+++ src/test/java/org/test/NewTasks.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class NewTasks {
+	private void add() {
+		System.out.println("nallavan");
+	}
+public static void main(String[] args) {
+	NewTasks n = new NewTasks();
+	n.add();
+}
+}
